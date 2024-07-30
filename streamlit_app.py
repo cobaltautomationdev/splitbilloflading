9915f23dbf09b3604c4588333e2a7305f19ae7bb
cobaltautomationdev@gmail.com <cobaltautomationdev@gmail.com>
2024-07-30 10:33:39 +0800

    Changes to be committed: 	modified:   streamlit_app.py
    reconstruct code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,118 +1,162 @@
 import requests
 import time
-
 import zipfile
 import io
-
 import streamlit as st
+import json
 
-
-def split_pdf_by_bol_number(uploaded_file):
-
-    username = st.secrets["username_abby"]
-    password =st.secrets["password_abby"]
-
-    client_id = st.secrets["CLIENT_ID"]
-    client_secret =st.secrets["CLIENT_SECRET"]
-    
-    # get access token
+def get_access_token(username, password, client_id, client_secret):
     url = "https://vantage-au.abbyy.com/auth2/connect/token"
-    data = {"grant_type":"password","scope":"openid permissions global.wildcard",
-            "username":{username},
-            "password":{password},
-            "client_id":{client_id},
-            "client_secret":{client_secret}}
-    headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
+    data = {
+        "grant_type": "password",
+        "scope": "openid permissions global.wildcard",
+        "username": username,
+        "password": password,
+        "client_id": client_id,
+        "client_secret": client_secret
     }
-
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     response = requests.post(url, headers=headers, data=data)
     if response.status_code == 200:
-        get_access_token = response.json()["access_token"]
+        return response.json()["access_token"]
     else:
         print("Error:", response.status_code, response.text)
-      
-    # get transaction id  
+        return None
+
+def create_transaction(access_token):
     api_url = 'https://vantage-au.abbyy.com/api/publicapi/v1/transactions'
-    data = {"skillId": "e9e6517a-7a19-4f73-b2e4-857227f904f4",
-            "generateMobileInputLink": False,
-            "registrationParameters": [
-                            {
-                                "key": "string",
-                                "value": "string"
-                            }
-                                    ]
+    data = {
+        "skillId": "e9e6517a-7a19-4f73-b2e4-857227f904f4",
+        "generateMobileInputLink": False,
+        "registrationParameters": []
     }
+    headers = {'accept': 'text/plain', 'Authorization': f'Bearer {access_token}'}
+    response = requests.post(api_url, headers=headers, json=data)
+    return response.json()['transactionId']
 
-    headers = {'accept': 'text/plain','Authorization':'Bearer '+ get_access_token}
-    response = requests.post(api_url,headers=headers,json=data)
-    transactionID = response.json()['transactionId']
-
-    # upload pdf file
-    api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transactionID}/files'
-    headers = { 'accept': '*/*','Authorization':'Bearer '+ get_access_token}
-    model = {'Model':(None, 
-            '{"files": [{"index": 0,"imageProcessingOptions": {"autoCrop": "Default","autoOrientation": "Default"},"registrationParameters": [{"key": "string","value": "string"}]}]}')}
-    
-    file = {
-        'file': (uploaded_file.name, uploaded_file.read(), 'application/pdf')
+def upload_file(transaction_id, access_token, file):
+    api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transaction_id}/files'
+    headers = {'accept': '*/*', 'Authorization': f'Bearer {access_token}'}
+    model = {'Model': (None, '{"files": [{"index": 0,"imageProcessingOptions": {"autoCrop": "Default","autoOrientation": "Default"},"registrationParameters": []}]}' )}
+    file_data = {
+        'file': (file.name, file.read(), 'application/pdf')
     }
-    response = requests.post(api_url,headers=headers,data = model,files=file,)
+    response = requests.post(api_url, headers=headers, data=model, files=file_data)
+    return response
 
-    # start transactions
-    api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transactionID}/start'
-    response = requests.post(api_url,headers=headers,)
-     
-    # check if the transaction is processed
-    api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transactionID}'
-    response = requests.get(api_url,headers=headers,)
-    status = response.json()['status']
+def start_transactions(transaction_id, access_token):
+    api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transaction_id}/start'
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = requests.post(api_url, headers=headers)
+    return response
 
-    while status != 'Processed':
-        time.sleep(5)
-        response = requests.get(api_url,headers=headers,)
+def wait_for_processing(transaction_id, access_token):
+    api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transaction_id}'
+    headers = {'Authorization': f'Bearer {access_token}'}
+    while True:
+        response = requests.get(api_url, headers=headers)
         status = response.json()['status']
-    
-    headers = { 'accept': 'application/octet-stream','Authorization':'Bearer '+ get_access_token}
-    zip_buffer = io.BytesIO()
-    
-    with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-        for document in response.json()['documents']:
-            file_id_json = document['resultFiles'][0]['fileId']
-            api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transactionID}/files/{file_id_json}/download'
-            response_json = requests.get(api_url,headers=headers,)
-            result = response_json.json()
-            
-            try:
-                bill_of_lading = result['Fields']['Bill of Lading']
-            except KeyError:
-                # Handle error: if there is an issue with the field, use the default value
-                print(f"Warning: 'Bill of Lading' field not found for document with fileId: {file_id_json}")
-                bill_of_lading = "not bill of lading"  # Use the default value
-            
-            # bill_of_lading = result['Fields']['Bill of Lading']
-            
-            file_id_pdf = document['resultFiles'][1]['fileId']
-            api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transactionID}/files/{file_id_pdf}/download'
-            response_pdf = requests.get(api_url,headers=headers,)
-            zip_file.writestr(f"{bill_of_lading}.pdf", response_pdf.content)
-            
-    zip_filename = uploaded_file.name.replace(".pdf", ".zip")
-    # 将 ZIP 文件的内容提供给 Streamlit 用于下载
-    st.download_button(
-        label="Download All PDFs",
-        data=zip_buffer.getvalue(),
-        file_name=zip_filename,
-        mime="application/zip"
-    )
-          
-               
-st.set_page_config(page_title="Split pdf file by bill of lading No", layout="wide",page_icon="icon\cobalt_logo.png") 
-st.sidebar.header("Split pdf file by bill of lading No")
-    
-with st.sidebar.form("pdfselectForm",clear_on_submit=True):
-    uploaded_file  = st.file_uploader("Choose PDF file(s)",accept_multiple_files=False, type="pdf")
-    submit_button = st.form_submit_button("Split pdf file(s)")
+        if status == 'Processed':
+            break
+        time.sleep(5)
 
-if submit_button and uploaded_file:
-    split_pdf_by_bol_number(uploaded_file)
+def download_processed_files(transaction_id, access_token):
+    api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transaction_id}'
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = requests.get(api_url, headers=headers)
+    documents = response.json()['documents']
+    return documents
+
+def get_bill_of_lading(result):
+    try:
+        return result['Fields']['Bill of Lading']
+    except KeyError:
+        print(f"Warning: 'Bill of Lading' field not found.")
+        return "not bill of lading"
+
+def download_file(file_id, transaction_id, access_token):
+    api_url = f'https://vantage-au.abbyy.com/api/publicapi/v1/transactions/{transaction_id}/files/{file_id}/download'
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = requests.get(api_url, headers=headers)
+    if response.status_code == 200:
+        return response.content
+    else:
+        print("Error:", response.status_code, response.text)
+        return None
+
+def process_pdf_file(uploaded_file):
+    access_token = get_access_token(st.secrets["username_abby"], st.secrets["password_abby"],
+                                    st.secrets["CLIENT_ID"], st.secrets["CLIENT_SECRET"])
+    if not access_token:
+        st.error("Failed to get access token.")
+        return
+
+    transaction_id = create_transaction(access_token)
+    if not transaction_id:
+        st.error("Failed to create transaction.")
+        return
+
+    response = upload_file(transaction_id, access_token, uploaded_file)
+    if response.status_code != 200:
+        st.error(f"Failed to upload file: {response.status_code} - {response.text}")
+        return
+
+    start_transactions(transaction_id, access_token)
+
+    wait_for_processing(transaction_id, access_token)
+
+    documents = download_processed_files(transaction_id, access_token)
+
+    zip_buffer = io.BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        for document in documents:
+            file_id_json = document['resultFiles'][0]['fileId']
+            json_content = download_file(file_id_json, transaction_id, access_token)
+            if json_content:
+                result = json.loads(json_content)
+                bill_of_lading = get_bill_of_lading(result)
+
+            file_id_pdf = document['resultFiles'][1]['fileId']
+            pdf_content = download_file(file_id_pdf, transaction_id, access_token)
+            if pdf_content:
+                zip_file.writestr(f"{bill_of_lading}.pdf", pdf_content)
+
+    zip_buffer.seek(0)
+    return zip_buffer
+
+def process_pdf_files(uploaded_files):
+    if len(uploaded_files) == 1:
+        zip_filename = uploaded_files[0].name.replace(".pdf", ".zip")
+        zip_buffer = process_pdf_file(uploaded_files[0])
+        st.download_button(
+            label=f"Download '{zip_filename}' file",
+            data=zip_buffer.getvalue(),
+            file_name=zip_filename,
+            mime="application/zip"
+        )
+    else:
+        zip_buffer_all = io.BytesIO()
+        with zipfile.ZipFile(zip_buffer_all, 'w', zipfile.ZIP_DEFLATED) as zip_file_all:
+            for i, uploaded_file in enumerate(uploaded_files):
+                zip_buffer = process_pdf_file(uploaded_file)
+                if zip_buffer:
+                    zip_filename = uploaded_file.name.replace(".pdf", ".zip")
+                    zip_file_all.writestr(zip_filename, zip_buffer.getvalue())
+
+        zip_filename = "all_zips.zip"
+        st.download_button(
+            label="Download All ZIPs",
+            data=zip_buffer_all.getvalue(),
+            file_name=zip_filename,
+            mime="application/zip"
+        )
+
+st.set_page_config(page_title="Split pdf files by bill of lading No", layout="wide")
+st.sidebar.header("Split pdf files by bill of lading No")
+
+with st.sidebar.form("pdfselectForm", clear_on_submit=True):
+    uploaded_files = st.file_uploader("Choose PDF files", accept_multiple_files=True, type="pdf")
+    submit_button = st.form_submit_button("Split pdf files")
+
+if submit_button and uploaded_files:
+    process_pdf_files(uploaded_files)
