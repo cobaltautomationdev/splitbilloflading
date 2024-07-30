1faa35902d7efdc7c1f434cf04d7b3b753e92638
cobaltautomationdev@gmail.com <cobaltautomationdev@gmail.com>
2024-07-30 14:46:30 +0800

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,6 +151,49 @@ def process_pdf_files(uploaded_files):
             mime="application/zip"
         )
 
+
+import hmac
+import streamlit as st
+
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
 st.set_page_config(page_title="Split pdf files by bill of lading No", layout="wide")
 st.sidebar.header("Split pdf files by bill of lading No")
 
